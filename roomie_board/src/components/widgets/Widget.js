6512944f0031daae69e20ac488df4adc4bc05655
lcm9995 <lcm9995@nyu.edu>
2025-12-02 01:38:26 -0500

    added draggable

--- roomie_board/src/components/widgets/Widget.js
+++ roomie_board/src/components/widgets/Widget.js
@@ -1,19 +1,22 @@
 import "./Widget.css";
-
+import Draggable from "react-draggable";
+import {useRef} from "react";
 export default function Widget(props) {
   const { title, children, action, className } = props;
+  const ref = useRef(null);
   return (
-    <div className={`widget-base ${className}`}>
-      <div className="widget-title-row">
-        <h2 className="widget-title">{title}</h2>
-        {action && (
-          <button className="widget-action" onClick={action.onClick}>
-            {action.label}
-          </button>
-        )}
+    <Draggable nodeRef={ref} handle=".drag-handle">
+        <div ref={ref} className={`widget-base ${className}`}>
+          <div className="widget-title-row drag-handle">
+            <h2 className="widget-title">{title}</h2>
+            {action && (
+              <button className="widget-action" onClick={action.onClick}>
+                {action.label}
+              </button>
+            )}
+          </div>
+        {children}
       </div>
-
-      {children}
-    </div>
+     </Draggable>
   );
 }
